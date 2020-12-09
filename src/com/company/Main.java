35200289bfbebbc666c41d938ab8589a515dc631
semen-nenashev <nenashev.sd@gmail.com>
2020-12-09 11:13:14 +0200

    fixing codestyle issues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,42 +3,43 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-enum Season{
+
+enum Season {
     winter, spring, summer, autumn
 }
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Print a month");
         String input = br.readLine();
         Season season;
         season = Season.winter;
-        switch (input.toLowerCase()){
+        switch (input.toLowerCase()) {
             case "december":
             case "january":
             case "february":
                 season = Season.winter;
-                System.out.println("It's "+season+ " now");
+                System.out.println("It's " + season + " now");
                 break;
             case "march":
             case "april":
             case "may":
                 season = Season.spring;
-                System.out.println("It's "+season+ " now");
+                System.out.println("It's " + season + " now");
                 break;
             case "june":
             case "july":
             case "august":
                 season = Season.summer;
-                System.out.println("It's "+season+ " now");
+                System.out.println("It's " + season + " now");
                 break;
             case "september":
             case "october":
             case "november":
                 season = Season.autumn;
-                System.out.println("It's "+season+ " now");
+                System.out.println("It's " + season + " now");
                 break;
             default:
                 System.out.println("Print correct month!");
